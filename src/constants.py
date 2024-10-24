f1c4dcc09271d99dc5b50a5b4a3c3e03336cb440
wmbelk <61924388+wmbelk@users.noreply.github.com>
2024-10-24 16:35:07 -0500

    Updated CAN addresses for Purple haze as built

--- src/constants.py
+++ src/constants.py
@@ -74,11 +74,35 @@ class CAN_Address(IntEnum):
     TEN = auto()
     ELEVEN = auto()
     TWELVE = auto()
+    THIRTEEN = auto()
+    FOURTEEN = auto()
+    FIFTEEN = auto()
+    SIXTEEN = auto()
+    SEVENTEEN = auto()
+    EIGHTEEN = auto()
+    NINETEEN = auto()
+    TWENTY = auto()
 
 #endregion
 
 class DriveConstant:
-    ...
+    # Ports for Purple Haze 10/24/2024 (not all captured)
+    kLeftMotorFront_DrivePort = CAN_Address.TWO
+    kLeftMotorBack_DrivePort = CAN_Address.FOUR
+    kRightMotorFront_DrivePort = CAN_Address.EIGHT
+    kRightMotorBack_DrivePort = CAN_Address.SIX
+    kclimbMotorPort = CAN_Address.TEN
+
+    kLeftMotorFront_SteerPort = CAN_Address.ONE 
+    kLeftMotorBack_SteerPort = CAN_Address.THREE
+    kRightMotorFront_SteerPort = CAN_Address.SEVEN
+    kRightMotorBack_SteerPort = CAN_Address.FIVE
+
+    kLeftAimingMotorPort = CAN_Address.FIFTEEN
+    kRightAimingMotorPort = CAN_Address.FOURTEEN
+
+
+
 
 
 class OIConstant:
